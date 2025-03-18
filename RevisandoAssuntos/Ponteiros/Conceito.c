e1e011378637ee7110181b2beba6c99d438d1eba
Lucas <vinicioslucas37@hotmail.com>
2025-03-18 20:31:54 -0300

    Conceito de ponteiros.

---
+++ RevisandoAssuntos/Ponteiros/Conceito.c
@@ -0,0 +1,12 @@
+/*Assim como declaramos variavéis para os tipos int, float, boolean, double.. Podemos
+criar variáveis do tipo Ponteiro que armazenam um endereço de memória que suporta um outro
+tipo de dado.
+
+ex>
+
+int a = 4; uma variável do tipo int.
+
+int *b; uma variável do tipo ponteiro, no qual suporta o tipo de dado int, ou seja, b só pode
+trabalhar com valores inteiros!!
+
+*/
