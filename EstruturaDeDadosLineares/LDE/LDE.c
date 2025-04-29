1042cb83a04316979b9013893f9c47b44e7e04cd
Lucas Vinícius <148213832+Lucavinini@users.noreply.github.com>
2025-04-29 10:38:01 +0000

    Aprendendo LDE

---
+++ EstruturaDeDadosLineares/LDE/LDE.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no{
+	
+	struct no *anterior;
+	int valor;
+	struct no *prox;
+	
+}No;
+
+typedef struct desc{
+	
+	struct no *inicio;
+	int qtd = 0;
+	struct no *fim;	
+	
+}Descritor;
+
+void Inicializar(Descritor **mydesc){
+	
+	if(mydesc->qtd == 0){
+		mydesc->inicio = NULL;
+		mydesc->prox = NULL;
+	}
+	else{
+		return;	
+	}	
+	
+}
+
+void InserirNoInicio(Descritor **mydesc, int valor){
+	
+	Inicializar();
+	
+	//Descritor vazio
+	if(mydesc->qtd == 0){
+		
+		No *MeuNo;
+		MeuNo = (No*)malloc(sizeof(MeuNo));
+		
+		MeuNo->anterior = NULL;
+		MeuNo->prox = NULL;
+		MeuNo->valor = valor;
+		
+		mydesc->inicio = MeuNo
+		mydesc->qtd++;
+		mydesc->fim = MeuNo;	
+	}
+	else{ //Lista com elementos
+	
+	No *aux;
+	aux = mydesc->inicio;
+	
+	No *MeuNo;
+	MeuNo = (No)malloc(sizeof(No));
+	
+	MeuNo->anterior = NULL;
+	MeuNo->valor = valor;
+	MeuNo->prox = NULL;
+	
+	mydesc->incio = MeuNo;
+	MeuNo->prox = *aux;
+	mydesc->qtd++;
+	
+	aux->anterior = MeuNo;		
+	}	
+}
+
+void InserirNoFinal(Descritor **mydesc, int valor){
+	
+	Inicializar(&mydesc)
+		
+	if(mydesc->qtd == 0){
+		
+		No *MeuNo;
+		MeuNo=(No*)malloc(sizeof(No))
+		
+		MeuNo->anterior = NULL;
+		MeuNo->prox= NULL;
+		MeuNo->valor = valor;
+		
+		mydesc->inicio = MeuNo;
+		mydesc->fim = MeuNo;
+		mydesc->qtd++;	
+	}
+	else{
+	
+		No *aux;
+		aux = mydesc->fim;
+		
+		No *MeuNo;
+		MeuNo = (No*)malloc(sizeof(No));
+		
+		MeuNo->anterior = *aux;
+		MeuNo->prox = NULL;
+		MeuNo->valor = valor;
+		
+		aux->prox = MeuNo;
+		
+		mydesc->qtd++;
+		
+	}	
+}
