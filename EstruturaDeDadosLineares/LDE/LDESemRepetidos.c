4c2e00d1523e3acbcbf21de90e79d2194621312a
Lucas Vinícius <148213832+Lucavinini@users.noreply.github.com>
2025-05-19 17:09:35 +0000

    Finalizando a 2 parte.

--- EstruturaDeDadosLineares/LDE/LDESemRepetidos.c
+++ EstruturaDeDadosLineares/LDE/LDESemRepetidos.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#DEFINE TRUE 1;
-#DEFINE FALSE 0;
+#define TRUE 1;
+#define FALSE 0;
 
 typedef struct no{
 
@@ -20,7 +20,7 @@ typedef struct desc{
 
 //Inicializar Lista
 
-void Inicializar(**Lista){
+void Inicializar(Descritor **Lista){
  
     Lista=(Descritor*)malloc(sizeof(Descritor));
     (*Lista)->inicio = NULL;
@@ -38,8 +38,7 @@ void IsEmpty(*Lista){
     }
 }
 
-
-// a)
+// a) 
 //Função que retorna o endereço de um número procurado
 
 No *RetornarEndereco(Descritor **Lista, int value){
@@ -67,34 +66,130 @@ No *RetornarEndereco(Descritor **Lista, int value){
 
 void InserirNoInicio(Descritor **Lista, int value){
 
-    //Verificar Repetido
-
     No *aux;
     aux = (*Lista)->inicio;
+
+    No *NovoNo;
+    NovoNo = (No*)malloc(sizeof(No));
+
+    NovoNo->ant = NULL;
+    NovoNo->info = value;
+    NovoNo->prox = NULL;
 
     while(aux != NULL){
         if(aux->info == value){
             printf("Valor repetido\n");
-            return
+            return;
         }
         aux = aux->prox;
     }
 
     if(IsEmpty(*Lista) == TRUE){
 
+        (*Lista)->inicio = NovoNo;
+        (*Lista)->qtd++;
+        (*Lista)->fim = NovoNo;
+
+    }
+    else{
         
+        NovoNo->prox = (*Lista)->inicio;
+        (*Lista)->inicio->ant = NovoNo;
+        (*Lista)->inicio = NovoNo;
+        (*Lista)->qtd++;
     }
 
-   
 }
 
 
-
-
-
-
-
-
 //Inserir No final sem ser repetido;
+
+void InserirNoFinalSemRepetidos(Descritor **Lista, int value){
+
+    No *NovoNo;
+    NovoNo=(No*)malloc(sizeof(No));
+
+    NovoNo->ant = NULL;
+    NovoNo->info = value;
+    NovoNo->prox = NULL;
+
+    No *aux;
+    aux = (*Lista)->inicio;
+
+    while(aux != NULL){
+        if(aux->info == value){
+            printf("Repetido.\n");
+            return;
+        }
+        aux = aux->prox;
+    }
+
+    if(IsEmpty(*Lista) == TRUE){
+        
+        (*Lista)->inicio = NovoNo;
+        (*Lista)->fim = NovoNo;
+        (*Lista)->qtd++;
+
+    }
+    else{
+        NovoNo->ant = (*Lista)->fim;
+        (*Lista)->fim->prox = NovoNo;
+        (*Lista)->fim = NovoNo;
+        (*Lista)->qtd++;
+    }
+}
+
 //Exibir Todos os números da lista
+
+void ExibirElemetos(Descritor **Lista){
+
+    if(IsEmpty(*Lista) == TRUE){
+        printf("Lista sem elementos\n");
+        return;
+    }
+
+    No aux;
+    aux = (*Lista)->inicio;
+
+    while(aux != NULL){
+        printf("%d", aux->info);
+        aux = aux->prox;
+    }
+}
+
 //Remover um especifico usando a função a)
+
+void RemoverEspecifico(Descritor **Lista, int value){
+
+    if(RetornarEndereco(*Lista, value) == 0){
+        printf("Lista vazia\n");
+        return;
+    }
+
+    if(RetornarEndereco(*Lista, value) == NULL){
+        printf("Valor não existe na lista\n");
+        return;
+    }
+
+    No *aux;
+    aux = RemoverEspecifico(*Lista, value);
+
+    No *anterior;
+
+
+    if(aux->ant == NULL){
+
+        (*Lista)->inicio = (*Lista)->inicio->prox;
+        free(aux);
+        (*Lista)->qtd--;
+    }
+    else{
+        anterior = aux->ant;
+        anterior->prox = aux->prox;
+        free(aux);
+        (*Lista)->qtd--;
+    }
+
+}
+
+//Desalocar os nos
